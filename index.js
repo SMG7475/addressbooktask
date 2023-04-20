5f93dfcf07032d773be1f8ce7ef6f355dc6684c3
SMG7475 <mahammadghouse82@gmail.com>
2023-04-20 13:06:08 +0530

    used class

--- index.js
+++ index.js
@@ -1,261 +1,131 @@
-// const myInstance = new Services();
-// myInstance.myMethod();
+const myServices = new Services();
 var activeContact = null;
-
-// const form = $('form');
-
-
-
-// form.addEventListener('submit', (event) => {
-
-//     event.preventDefault();
-//     const nameInput = $('#name');
-//     const emailInput = $('#email');
-//     const mobileInput = $('#mobile');
-//     const landlineInput = $('#landline');
-//     const websiteInput = $('#website');
-//     const addressInput = $('#address');
-
-//     const nameValue = nameInput.value.trim();
-//     if (nameValue === '') {
-//         $("#name-errmsg").html('Please enter your name.');
-//         nameInput.focus();
-//         return;
+// function validateLandline() {
+//     let landlineValue = $("#landline").val();
+//     if (landlineValue.length == 0) {
+//         $("#landline-errmsg").show();
+//         return false
 //     }
-//     const nameRegex = /^[a-zA-Z\s'-]+$/;
-//     if (!nameRegex.test(nameValue)) {
-//         $("#name-errmsg").html('*Enter a valid name.');
-//         nameInput.focus();
-//         return;
-//     }
-//     if (nameValue.length < 3) {
-//         $("#name-errmsg").html('*Enter a name that is greater than 3 characters.');
-//         nameInput.focus();
-//         return;
-//     }
-// })    
-
-
-//     const emailValue = emailInput.value.trim();
-//     if (emailValue === '') {
-//         alert('Please enter your email address.');
-//         emailInput.focus();
-//         return;
-//     }
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     if (!emailRegex.test(emailValue)) {
-//         alert('Please enter a valid email address.');
-//         emailInput.focus();
-//         return;
-//     }
-//     if (emailValue.length > 100) {
-//         alert('Please enter an email address that is 100 characters or less.');
-//         emailInput.focus();
-//         return;
-//     }
-
-
-//     const mobileValue = mobileInput.value.trim();
-//     if (mobileValue === '') {
-//         alert('Please enter your mobile number.');
-//         mobileInput.focus();
-//         return;
-//     }
-//     const mobileRegex = /^\+?\d{1,3}[\s-]?\d{3}[\s-]?\d{3}[\s-]?\d{4}$/;
-//     if (!mobileRegex.test(mobileValue)) {
-//         alert('Please enter a valid mobile number.');
-//         mobileInput.focus();
-//         return;
-//     }
-//     if (mobileValue.length > 15) {
-//         alert('Please enter a mobile number that is 15 characters or less.');
-//         mobileInput.focus();
-//         return;
-//     }
-
-
-//     const landlineValue = landlineInput.value.trim();
-//     if (landlineValue !== '') {
-//         const landlineRegex = /^\+?\d{1,3}[\s-]?\d{3}[\s-]?\d{3}[\s-]?\d{4}$/;
-//         if (!landlineRegex.test(landlineValue)) {
-//             alert('Please enter a valid landline number.');
-//             landlineInput.focus();
-//             return;
-//         }
-//         if (landlineValue.length > 15) {
-//             alert('Please enter a landline number that is 15 characters or less.');
-//             landlineInput.focus();
-//             return;
-//         }
-//     }
-
-
-//   const websiteValue = websiteInput.value.trim();
-//   if (websiteValue === '') {
-//     alert('Please enter your website URL.');
-//     websiteInput.focus();
-//     return;
-//   }
-//   const websiteRegex = /^(https?:\/\/)?[^\s\/]+(\.[^\s\/]+)+[^\s]*$/;
-//   if (!websiteRegex.test)
-////
-function validateName() {
-    let nameValue = $("#name").val();
-    if (nameValue.length == 0) {
-        $("#name-errmsg").show();
-        $("#name-errmsg").html("*Name is required")
-        return false;
-    }
-    else if (nameValue.length < 3 ) {
-        $("#name-errmsg").show();
-        $("#name-errmsg").html("*length of name must be greater than 3 characters");
-        return false;
-    }
-    else {
-        $("#name-errmsg").hide();
-        return true
-    }
-}
-function validateMobile() {
-    let mobileValue = $("#mobile").val();
-    if (mobileValue.length == 0) {
-        $("#mobile-errmsg").show()
-        $("#mobile-errmsg").html("*Mobile Number is required")
-    }
-    else if (mobileValue.length != 10) {
-        $("#mobile-errmsg").show();
-        $("#mobile-errmsg").html("*Enter a valid Mobile Number");
-    }
-    else {
-        $("#mobile-errmsg").hide();
-        return true
-    }
-
-}
-function validateEmail(){
-    let emailValue = $("#email").val();
-    if (emailValue.length == 0){
-        $("#email-errmsg").show();
-        return false
-    }
-    else if (emailValue.includes("@") == false){
-        $("#email-errmsg").show();
-        $("#email-errmsg").html("Enter a Valid Email")
-        return false
-    }
-    else{
-        $("#email-errmsg").hide();
-        return true
-    }
-}
-function validateLandline(){
-    let landlineValue = $("#landline").val();
-    if (landlineValue.length == 0){
-        $("#landline-errmsg").show();
-        return false
-    }
-    $("#landline-errmsg").hide();
-    return true
-}
-function validateWebsite(){
-    let websiteValue = $("#website").val();
-    if (websiteValue.length == 0){
-        $("#website-errmsg").show();
-        return false
-    }
-    $("#website-errmsg").hide();
-    return true
-}
-function validateAddress(){
-    let addressValue = $("#address").val();
-    if (addressValue.length == 0){
-        $("#address-errmsg").show();
-        return false
-    }
-    $("#address-errmsg").hide();
-    return true
-}
-// function inputValidations(){
-//     let inputId = event.target.id;
-//     let inputValue = event.target.value;
-//     //console.log(inputId,inputValue)
-//     switch (inputId){
-//         case "name":
-//             if (inputValue.length == 0) {
-//                 $("#name-errmsg").show();
-//                 $("#name-errmsg").html("*Name is required")
-//                 return false;
-//             }
-//             else if (inputValue.length < 3) {
-//                 $("#name-errmsg").show();
-//                 $("#name-errmsg").html("*length of username must be greater than 3 characters");
-//                 return false;
-//             }
-//             else {
-//                 $("#name-errmsg").hide();
-//                 return true
-//             }
-//         case "email":
-//             if (inputValue.length == 0){
-//                 $("#email-errmsg").show();
-//                 return false
-//             }
-//             else if (inputValue.includes("@") == false){
-//                 $("#email-errmsg").show();
-//                 $("#email-errmsg").html("*Enter a Valid Email")
-//                 return false
-//             }
-//             else {
-//                 $("#email-errmsg").hide();
-//                 return true
-//             }
-//         case "mobile":
-//             if (inputValue.length == 0) {
-//                 $("#mobile-errmsg").show()
-//                 $("#mobile-errmsg").html("*Mobile Number is required")
-//                 return false
-//             }
-//             else if (inputValue.length != 10) {
-//                 $("#mobile-errmsg").show();
-//                 $("#mobile-errmsg").html("*Enter a valid Mobile Number");
-//                 return false
-//             }
-//             else {
-//                 $("#mobile-errmsg").hide();
-//                 return true
-//             }
-//         case "landline":
-//             if (inputValue.length == 0) {
-//                 $("#landline-errmsg").show();
-//                 return false
-//             }
-//             $("#landline-errmsg").hide();
-//             return true
-//         case "website":
-//             if (inputValue.length == 0){
-//                 $("#website-errmsg").show();
-//                 return false
-//             }
-//             $("#website-errmsg").hide();
-//             return true
-//         case "address":
-//             if (inputValue.length == 0){
-//                 $("#address-errmsg").show();
-//                 return false
-//             }
-//             $("#address-errmsg").hide();
-//             return true
-
-//     }    
-
+//     $("#landline-errmsg").hide();
+//     return true
 // }
+// function validateWebsite() {
+//     let websiteValue = $("#website").val();
+//     if (websiteValue.length == 0) {
+//         $("#website-errmsg").show();
+//         return false
+//     }
+//     $("#website-errmsg").hide();
+//     return true
+// }
+// function validateAddress() {
+//     let addressValue = $("#address").val();
+//     if (addressValue.length == 0) {
+//         $("#address-errmsg").show();
+//         return false
+//     }
+//     $("#address-errmsg").hide();
+//     return true
+// }
+function onAddValidateInputs() {
+    let onBtnId = event.target.id;
+    let validName = inputValidations("name", $("#name").val());
+    let validEmail = inputValidations("email", $("#email").val());
+    let validMobile = inputValidations("mobile", $("#mobile").val());
+    let validLandline = inputValidations("landline", $("#landline").val());
+    let validWebsite = inputValidations("website", $("#website").val());
+    let validAddress = inputValidations("address", $("#address").val());
+    if (validName && validMobile && validEmail && validLandline && validWebsite && validAddress) {
+        if (onBtnId == "form-add-button") {
+            formAddButton()
+        }
+        else if (onBtnId == "form-update-button"){
+            onUpdateContact()
+        }
+    }
+}
+
+function inputValidations(id, value) {
+    let inputId = id;
+    let inputValue = value;
+    if (id == undefined && value == undefined) {
+        inputId = event.target.id;
+        inputValue = event.target.value;
+    }
+    switch (inputId) {
+        case "name":
+            if (inputValue.length == 0) {
+                $("#name-errmsg").show();
+                $("#name-errmsg").html("*Name is required")
+                return false;
+            }
+            else {
+                $("#name-errmsg").hide();
+                return true
+            }
+        case "email":
+            const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+            let emailValue = $("#email").val();
+            if (emailValue.length == 0) {
+                $("#email-errmsg").show();
+                return false
+            }
+            if (!emailRegex.test(emailValue)) {
+                $("#email-errmsg").show();
+                $("#email-errmsg").html("Enter a Valid Email")
+                return false
+            }
+            else {
+                $("#email-errmsg").hide();
+                return true
+            }
+        case "mobile":
+            if (inputValue.length == 0) {
+                $("#mobile-errmsg").show()
+                $("#mobile-errmsg").html("*Mobile Number is required")
+                return false
+            }
+            else if (inputValue.length != 10) {
+                $("#mobile-errmsg").show();
+                $("#mobile-errmsg").html("*Enter a valid Mobile Number");
+                return false
+            }
+            else {
+                $("#mobile-errmsg").hide();
+                return true
+            }
+        case "landline":
+            if (inputValue.length == 0) {
+                $("#landline-errmsg").show();
+                return false
+            }
+            $("#landline-errmsg").hide();
+            return true
+        case "website":
+            if (inputValue.length == 0) {
+                $("#website-errmsg").show();
+                return false
+            }
+            $("#website-errmsg").hide();
+            return true
+        case "address":
+            if (inputValue.length == 0) {
+                $("#address-errmsg").show();
+                return false
+            }
+            $("#address-errmsg").hide();
+            return true
+
+    }
+
+}
 function createAndAppendAddrress(address) {
     $("#contacts-list").append(`<li id="${address.id}"><h2>${address.name}</h2><p>${address.email}</p><p>+91 ${address.mobile}</p></li>`)
 }
 function toDeleteContact() {
     $("#show-contact-details-container").hide();
     $('#contacts-list li').removeClass("contacts-list-selected-items")
-    let deleteUpdatedAddressBook = deleteContact(activeContact.id);
+    let deleteUpdatedAddressBook = myServices.deleteContact(activeContact.id);
     $("#contacts-list").html("")
     deleteUpdatedAddressBook.map(createAndAppendAddrress)
 }
@@ -271,6 +141,12 @@ function toBlankInputs() {
     $("#landline").val("")
     $("#website").val("")
     $("#address").val("")
+    $("#name-errmsg").hide();
+    $("#email-errmsg").hide();
+    $("#mobile-errmsg").hide();
+    $("#landline-errmsg").hide();
+    $("#website-errmsg").hide();
+    $("#address-errmsg").hide();
 }
 function onAddNavbar() {
     $("#show-contact-details-container").hide()
@@ -297,7 +173,7 @@ function onUpdateContact() {
         website: $("#website").val(),
         address: $("#address").val(),
     }
-    let editedAddressBook = updateContact(updatedContact)
+    let editedAddressBook = myServices.updateContact(updatedContact)
     $("#add-address-form-container").hide();
     $("#contacts-list").html("");
     editedAddressBook.map(createAndAppendAddrress)
@@ -319,7 +195,7 @@ function formAddButton() {
         website: newWebsite,
         address: newAddress
     }
-    addContact(newAddressToAdd);
+    myServices.addContact(newAddressToAdd);
     createAndAppendAddrress(newAddressToAdd)
     toBlankInputs()
     $("#add-address-form-container").hide();
@@ -338,76 +214,9 @@ function onEdit() {
     $("#website").val(activeContact.website);
     $("#address").val(activeContact.address);
 }
-function onAddContact() {
-    let validName = validateName()
-    let validMobile = validateMobile()
-    let validEmail = validateEmail()
-    let validLandline = validateLandline()
-    let validWebsite = validateWebsite()
-    let validAddress = validateAddress()
-    if (validName && validMobile && validEmail && validLandline && validWebsite && validAddress) {
-        formAddButton()
-    }
-}
 $(document).ready(function () {
-
-    // $("#form-add-button").on('click', (event) => {
-
-    //     event.preventDefault();
-    //     let nameInput = $('#name').val();
-    //     let emailInput = $('#email').val();
-    //     let mobileInput = $('#mobile').val();
-    //     let landlineInput = $('#landline').val();
-    //     let websiteInput = $('#website').val();
-    //     let addressInput = $('#address').val();
-    //     let nameValue = nameInput.trim();
-    //     const nameRegex = /^[a-zA-Z\s'-]+$/;
-    //     const mobileRegex = /^\+?\d{1,3}[\s-]?\d{3}[\s-]?\d{3}[\s-]?\d{4}$/;
-    //     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    //     if (nameValue === '') {
-    //         $("#name-errmsg").show();
-    //         $("#name-errmsg").html('Please enter your name.');
-    //     }
-    //     else if (!nameRegex.test(nameValue)) {
-    //         $("#name-errmsg").show();
-    //         $("#name-errmsg").html('*Enter a valid name.');
-    //     }
-    //     else{
-    //         $("#name-errmsg").hide();
-    //     }
-
-    //     const mobileValue = mobileInput.trim();
-    //     if (mobileValue === '') {
-    //         $("#mobile-errmsg").show();
-    //         $('#mobile-errmsg').html('Please enter your mobile number.');
-    //     } else if (mobileValue.length == 10) {
-    //         $("#mobile-errmsg").show();
-    //         $('#mobile-errmsg').html('Please enter a mobile number that is 15 characters or less.');
-    //     }
-    //     else if (!mobileRegex.test(mobileValue)) {
-    //         $("#mobile-errmsg").show();
-    //         $('#mobile-errmsg').html('Please enter a valid mobile number.');
-    //     }
-    //     else{
-    //         $("#mobile-errmsg").hide();
-    //     }
-    //     const emailValue = emailInput.trim();
-    //     if (emailValue === '') {
-    //         $("#email-errmsg").html('Please enter your email address.');
-    //         $("#email-errmsg").show();
-    //     }
-    //     else if (!emailRegex.test(emailValue)) {
-    //         $("#email-errmsg").html('Please enter a valid email address.');
-    //         $("#email-errmsg").show();
-       
-    //     }
-    //     else{
-    //         $("#email-errmsg").hide();
-    //     }
-        
-    // })
     $("#show-contact-details-container").hide()
-    var addressBook = getAddressBookFromLocalStorage();
+    var addressBook = myServices.getAddressBookFromLocalStorage();
     addressBook.map(createAndAppendAddrress)
     $("#contacts-list").on('click', 'li', function () {
         $("#show-contact-details-container").show();
@@ -416,7 +225,7 @@ $(document).ready(function () {
         $(this).addClass("contacts-list-selected-items")
         $("#add-address-form-container").hide()
         var activeContactId = $(this).attr("id")
-        activeContact = getSpecificObject(activeContactId)
+        activeContact = myServices.getSpecificObject(activeContactId)
         $("#show-name").html(`${activeContact.name}`)
         $("#show-email").html(`Email: ${activeContact.email}`)
         $("#show-mobile").html(`Mobile: ${activeContact.mobile}`)
--- services.js
+++ services.js
@@ -1,45 +1,45 @@
-// class Services{
-//     myMethod() {
-//         console.log('Hello, world!');
-//       }
-// }
-function getAddressBookFromLocalStorage() {
-    let stringifiedAddressBook = localStorage.getItem("addressBook");
-    let parsedAddressBook = JSON.parse(stringifiedAddressBook);
-    if (parsedAddressBook === null) {
-        return [];
-    } else {
-        return parsedAddressBook;
+class Services {
+    getAddressBookFromLocalStorage() {
+        let stringifiedAddressBook = localStorage.getItem("addressBook");
+        let parsedAddressBook = JSON.parse(stringifiedAddressBook);
+        if (parsedAddressBook === null) {
+            return [];
+        } else {
+            return parsedAddressBook;
+        }
+    } getSpecificObject(id) {
+        let addressBook = this.getAddressBookFromLocalStorage();
+        let getSpecificContact = addressBook.filter(function (address) {
+            return address.id == id
+        })
+        return getSpecificContact[0]
+    }
+    deleteContact(id) {
+        let addressBook = this.getAddressBookFromLocalStorage();
+        let deleteUpdatedAddressBook = addressBook.filter(function (address) {
+            return address.id != id
+        })
+        localStorage.setItem("addressBook", JSON.stringify(deleteUpdatedAddressBook));
+        return deleteUpdatedAddressBook
+    }
+    addContact(contact) {
+        let addressBook = this.getAddressBookFromLocalStorage();
+        addressBook.push(contact)
+        localStorage.setItem("addressBook", JSON.stringify(addressBook));
+    } updateContact(contact) {
+        let addressBook = this.getAddressBookFromLocalStorage();
+        let editedAddressBook = addressBook.map(function (address) {
+            if (address.id == contact.id) {
+                return contact
+            }
+            return address
+        })
+        localStorage.setItem("addressBook", JSON.stringify(editedAddressBook));
+        return editedAddressBook
     }
 }
-function getSpecificObject(id) {
-    let addressBook = getAddressBookFromLocalStorage();
-    let getSpecificContact = addressBook.filter(function (address) {
-        return address.id == id
-    })
-    return getSpecificContact[0]
-}
-function deleteContact(id) {
-    let addressBook = getAddressBookFromLocalStorage();
-    let deleteUpdatedAddressBook = addressBook.filter(function (address) {
-        return address.id != id
-    })
-    localStorage.setItem("addressBook", JSON.stringify(deleteUpdatedAddressBook));
-    return deleteUpdatedAddressBook
-}
-function addContact(contact){
-    let addressBook = getAddressBookFromLocalStorage();
-    addressBook.push(contact)
-    localStorage.setItem("addressBook", JSON.stringify(addressBook));
-}
-function updateContact(contact){
-    let addressBook = getAddressBookFromLocalStorage();
-    let editedAddressBook = addressBook.map(function (address) {
-        if (address.id == contact.id) {
-            return contact
-        }
-        return address
-    })
-    localStorage.setItem("addressBook", JSON.stringify(editedAddressBook));
-    return editedAddressBook
-}
+
+
+
+
+
